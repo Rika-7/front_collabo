1d7d857f010c8c027aea2ee0bb3b9279092fde13
Rika <>
2025-04-01 23:46:52 -0400

    Initial commit

--- src/app/project_registration/page.tsx
+++ src/app/project_registration/page.tsx
@@ -1,10 +1,13 @@
-export default function Home() {
+import { Header } from "@/components/common/Header";
+
+export default function Project_registration() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">研Q</h1>
-        <p className="text-lg">研究者と企業をつなぐプラットフォーム</p>
-        <p className="text-lg">産学連携用プラットフォーム</p>
+        <div className="min-h-screen bg-gray-50">
+          <Header currentPage="案件検索" />
+          <h1 className="text-4xl font-bold">案件登録</h1>
+        </div>
       </main>
     </div>
   );
