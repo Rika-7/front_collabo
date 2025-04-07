55afc7e43ea4ea6d47d3ee47947bb4707adf6d05
Rika <>
2025-04-07 02:49:31 -0400

    Revised server.js.

--- server.js
+++ server.js
@@ -3,29 +3,23 @@ import { parse } from "url";
 import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.WEBSITE_HOSTNAME || "0.0.0.0";
+const hostname = "0.0.0.0";
 const port = process.env.PORT || 3000;
 
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     try {
       const parsedUrl = parse(req.url, true);
-
       handle(req, res, parsedUrl);
     } catch (err) {
       console.error("Error occurred handling", req.url, err);
       res.statusCode = 500;
       res.end("Internal Server Error");
     }
-  })
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, hostname, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
-    });
+  }).listen(port, hostname, () => {
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
 });
